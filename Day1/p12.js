4161a5a06b7786f1f5804e6764478c5e2417a1c2
thekhushi09 <thekhushitomar10@gmail.com>
2025-02-14 12:15:04 +0530

    new

---
+++ Day1/p12.js
@@ -0,0 +1,9 @@
+const fs=require('fs/promises');
+
+const read=async()=>{
+    const data=await fs.readFile("./data.txt","utf8");
+    console.log(data);
+}
+
+read();
+console.log("I am after reading data");
